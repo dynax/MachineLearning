2124555f1e5db4fd7c2893ee80d5deb7e991ec12
yaochen <yaochen@ualberta.ca>
2018-10-28 23:09:26 -0600

    updage

---
+++ mnist.py
@@ -0,0 +1,18 @@
+import tensorflow as tf 
+
+def loadData(path):
+	"""
+	[trainData, trainLabel, testData, testLabel] = loadData(path)
+		Load the minist data
+	inputs:
+		path	path to the data folder containing the mnist data with original file names
+
+	outputs:
+
+	"""
+	pass
+
+if __name__ == "main":
+	pass
+
+	
