d23ae460fa0caccee02d14d339921f2796328891
yaochen <yaochen@ualberta.ca>
2019-06-16 15:58:43 -0600

    Start to learn pytorch

--- download.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# download the remote result to my machine
-scp -r yaochen@142.244.63.49:MachineLearning/result .
--- upload.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# upload the local code to my remote machine
-scp mnist.py yaochen@142.244.63.49:MachineLearning/mnist.py
