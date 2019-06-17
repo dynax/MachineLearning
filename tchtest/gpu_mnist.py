2e04ae3da3b91a0d3ef7b0920748fc910a7aee37
yaochen <yaochen@ualberta.ca>
2019-06-17 14:56:56 -0600

    s

--- tchtest/gpu_mnist.py
+++ tchtest/gpu_mnist.py
@@ -84,7 +84,7 @@ correct = 0.
 total = 0.
 with torch.no_grad():
     for data in testloader:
-        images, labels = data
+        images, labels = data[0].to(device), data[1].to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
