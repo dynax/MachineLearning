2e751925a81c54146ed9170e63fc6081df956586
yaochen <yaochen@ualberta.ca>
2019-06-17 15:08:31 -0600

    s

--- tchtest/gpu_mnist.py
+++ tchtest/gpu_mnist.py
@@ -40,9 +40,9 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.1307, ), (0.3081, ))])
 trainset = torchvision.datasets.MNIST(root=path_data_folder, train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers).to(device)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True,)
 testset = torchvision.datasets.MNIST(root=path_data_folder, train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers).to(device)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True,)
 classes = ('0', '1', '2', '3','4', '5', '6', '7', '8', '9')
 
 # # get some random training images
