5bf162e5964f161478c7f0c8f4bda251e2c27051
yaochen <yaochen@ualberta.ca>
2019-06-17 15:29:26 -0600

    sync

--- tftest/gpu_mnist_keras.py
+++ tftest/gpu_mnist_keras.py
@@ -1,23 +1,38 @@
+
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 import time
+from tensorflow.keras import backend as K
 
-with tf.device("/gpu:0"):
-	(x_train, y_train),(x_test, y_test) = mnist.load_data()
-	x_train, x_test = x_train / 255.0, x_test / 255.0
+num_cores = 8
 
-	print("cnn model")
-	model = tf.keras.models.Sequential([
-	  tf.keras.layers.Flatten(input_shape=(28, 28)),
-	  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-	  tf.keras.layers.Dropout(0.2),
-	  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-	])
-	model.compile(optimizer='adam',
-	              loss='sparse_categorical_crossentropy',
-	              metrics=['accuracy'])
+num_CPU = 1
+num_GPU = 2
 
-	start_time = time.time()
-	model.fit(x_train, y_train, epochs=5, batch_size=128)
-	model.evaluate(x_test, y_test)
-	print(time.time()-start_time)
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
+                        inter_op_parallelism_threads=num_cores, 
+                        allow_soft_placement=True,
+                        device_count = {'CPU' : num_CPU,
+                                        'GPU' : num_GPU}
+                       )
+session = tf.Session(config=config)
+K.set_session(session)
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+print("cnn model")
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+start_time = time.time()
+model.fit(x_train, y_train, epochs=5, batch_size=128)
+model.evaluate(x_test, y_test)
+print(time.time()-start_time)
